fd4cc9d00a188280c5b3c5006afecbf46c21bee3
Gowthamkantubothu <gowthamkantubothu77@gmail.com>
2025-09-11 18:08:01 +0530

    updated

--- backend/candidates/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Candidate
-
-class CandidateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Candidate
-        fields = '__all__'
